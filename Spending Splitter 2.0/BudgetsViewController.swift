60c2342289562294256c03bb8cc714a67586ae86
Calvin Chestnut <calvin@Calvins-MacBook.local>
2016-10-11 15:42:14 -0400

    Start working on budget details page

---
+++ Spending Splitter 2.0/BudgetsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BudgetsViewController.swift
+//  Spending Splitter 2.0
+//
+//  Created by Calvin Chestnut on 9/8/16.
+//  Copyright © 2016 Calvin Chestnut. All rights reserved.
+//
+
+import Foundation
---
+++ Spending Splitter 2.0/ExpenseDetailCell.swift
@@ -0,0 +1,9 @@
+//
+//  ExpenseDetailCell.swift
+//  Spending Splitter 2.0
+//
+//  Created by Calvin Chestnut on 9/13/16.
+//  Copyright © 2016 Calvin Chestnut. All rights reserved.
+//
+
+import Foundation
